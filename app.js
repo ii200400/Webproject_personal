5791d68ba700d81b2ebfbe6421bae04f6e58831e
ii200400 <ii200400@naver.com>
2017-12-10 00:39:41 +0900

    게시판에 글보이기는 가능, dateformat사용 방법이 안나옴

--- app.js
+++ app.js
@@ -5,68 +5,10 @@ var favicon = require('serve-favicon');
 //var cookieParser = require('cookie-parser'); //cookie
 var bodyParser = require('body-parser'); //post
 
-//이 아래는 내가 개인적으로 추가한 것
-var mysql = require('mysql');
-var dateFormat = require('dateformat');
-
 var index = require('./routes/index');
 var admin = require('./routes/admin');
 
 var app = express();
-//수정 필요
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'dudtjs972972',
-  database : 'pilates'
-});
-
-connection.connect();
-
-var now = new Date();
-var time = dateFormat(now, "yyyy-mm-dd hh:MM:ss");
-console.log(time);
-// var insert_sql = 'INSERT INTO notice_table (name,title,description,created) VALUES(?,?,?,?)'
-// var insert_params = ['뇌지','재목제목','글을 씁니다.',time]
-// connection.query(insert_sql, params, function(err, rows, fields) {
-//   if (!err){
-//     console.log('The solution is: ', rows);
-//     for (var i = 0; i < rows.length; i++) {
-//       console.log(rows[i].title);
-//     }
-//     //rows.insertId 고유 id값을 가져올 수 있다고 한다.
-//   }else{
-//     console.log('Error while performing Query.', err);
-//   }
-// });
-
-// var update_sql = 'UPDATE notice_table SET name=?, title=? WHERE id=?'
-// var update_params = ['영선','업데이트!',1];
-// connection.query(update_sql, update_params, function(err, rows, fields) {
-//   if (!err){
-//     console.log('The solution is: ', rows);
-//     for (var i = 0; i < rows.length; i++) {
-//       console.log(rows[i].title);
-//     }
-//   }else{
-//     console.log('Error while performing Query.', err);
-//   }
-// });
-
-// var delete_sql = 'DELETE FROM notice_table WHERE id=?'
-// var delete_params = [2];
-// connection.query(delete_sql, delete_params, function(err, rows, fields) {
-//   if (!err){
-//     console.log('The solution is: ', rows);
-//     for (var i = 0; i < rows.length; i++) {
-//       console.log(rows[i].title);
-//     }
-//   }else{
-//     console.log('Error while performing Query.', err);
-//   }
-// });
-
-connection.end();
 
 // view engine (뷰 환경설정)
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,59 @@
 var express = require('express');
+
+var mysql = require('mysql');
+var dateFormat = require('dateformat');
+
+//수정 필요
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'dudtjs972972',
+  database : 'pilates'
+});
+
+var now = new Date();
+var time = dateFormat(now, "yyyy-mm-dd hh:MM:ss");
+console.log(time);
+// var insert_sql = 'INSERT INTO notice_table (name,title,description,created) VALUES(?,?,?,?)'
+// var insert_params = ['뇌지','재목제목','글을 씁니다.',time]
+// connection.query(insert_sql, params, function(err, rows, fields) {
+//   if (!err){
+//     console.log('The solution is: ', rows);
+//     for (var i = 0; i < rows.length; i++) {
+//       console.log(rows[i].title);
+//     }
+//     //rows.insertId 고유 id값을 가져올 수 있다고 한다.
+//   }else{
+//     console.log('Error while performing Query.', err);
+//   }
+// });
+
+// var update_sql = 'UPDATE notice_table SET name=?, title=? WHERE id=?'
+// var update_params = ['영선','업데이트!',1];
+// connection.query(update_sql, update_params, function(err, rows, fields) {
+//   if (!err){
+//     console.log('The solution is: ', rows);
+//     for (var i = 0; i < rows.length; i++) {
+//       console.log(rows[i].title);
+//     }
+//   }else{
+//     console.log('Error while performing Query.', err);
+//   }
+// });
+
+// var delete_sql = 'DELETE FROM notice_table WHERE id=?'
+// var delete_params = [2];
+// connection.query(delete_sql, delete_params, function(err, rows, fields) {
+//   if (!err){
+//     console.log('The solution is: ', rows);
+//     for (var i = 0; i < rows.length; i++) {
+//       console.log(rows[i].title);
+//     }
+//   }else{
+//     console.log('Error while performing Query.', err);
+//   }
+// });
+
 var router = express.Router();
 
 /* GET home page. */
@@ -9,6 +64,7 @@ router.get('/work', function(req, res, next) {
   res.render('work');
 });
 router.get('/work2', function(req, res, next) {
+  connection.connect();
   var search_sql = 'SELECT * FROM notice_table'
   connection.query(search_sql, function(err, rows, fields) {
     if (!err){
@@ -17,6 +73,7 @@ router.get('/work2', function(req, res, next) {
       console.log('Error while performing Query.', err);
     }
   });
+  connection.end();
 });
 router.get('/services', function(req, res, next) {
   res.render('services');
