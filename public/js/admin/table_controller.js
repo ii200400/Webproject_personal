930cabc9cfa771f6617ef2726014fd205b29e1d3
ii200400 <ii200400@naver.com>
2017-12-18 17:54:07 +0900

    textarea사용 및 코드 정리

--- public/js/admin/table_controller.js
+++ public/js/admin/table_controller.js
@@ -1,7 +1,7 @@
 var rows = document.getElementsByClassName("consult");
 
 function detail_page(self){
-  location.href = "/admin/consulting/" + (i - self.rowIndex);
+  location.href = "/admin/menu/consulting/" + (i - self.rowIndex);
 };
 for (var i = 0; i < rows.length; i++) {
   console.log(i);
--- routes/admin.js
+++ routes/admin.js
@@ -13,10 +13,20 @@ connection.connect();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('basic');
+  res.render('login');
 });
-router.get('/consulting', function(req, res, next) {
-  var search_sql = 'SELECT * FROM notice_table';
+router.get('/menu', function(req, res, next) {
+  var search_sql = 'SELECT * FROM numbersof';
+  connection.query(search_sql, function(err, rows, fields) {
+    if (!err){
+      res.render('basic', {numbers:rows[0]});
+    }else{
+      console.log('Error while performing Query.', err);
+    }
+  });
+});
+router.get('/menu/consulting', function(req, res, next) {
+  var search_sql = 'SELECT * FROM consult';
   connection.query(search_sql, function(err, rows, fields) {
     if (!err){
       res.render('consulting', {rows:rows});
@@ -25,8 +35,8 @@ router.get('/consulting', function(req, res, next) {
     }
   });
 });
-router.get('/consulting/:id', function(req, res, next) {
-  var search_sql = 'SELECT * FROM notice_table';
+router.get('/menu/consulting/:id', function(req, res, next) {
+  var search_sql = 'SELECT * FROM consult';
   var number = req.params.id;
   connection.query(search_sql, function(err, rows, fields) {
     if (!err){
@@ -40,11 +50,11 @@ router.get('/consulting/:id', function(req, res, next) {
 //서버에서만 사용
 router.post('/update', function(req, res, next) {
   var answer = req.body.answer.replace(/ /gi, "")===""? null : req.body.answer
-  var update_sql = 'UPDATE notice_table SET answer=? WHERE id=?'
+  var update_sql = 'UPDATE consult SET answer=? WHERE id=?'
   var update_params = [answer, req.body.id]
   connection.query(update_sql, update_params, function(err, rows, fields) {
     if (!err){
-      res.redirect('/admin/consulting')
+      res.redirect('/admin/menu/consulting')
     }else{
       console.log('Error while performing Query.', err);
     }
