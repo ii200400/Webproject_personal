b5696962c0d5890380ef313b3375908a4e5f9a4f
ii200400 <ii200400@naver.com>
2017-12-22 05:16:24 +0900

    디비연결 및 약간의 정리

--- routes/index.js
+++ routes/index.js
@@ -4,40 +4,34 @@ var router = express.Router();
 var connection = require('../db')();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/main', function(req, res, next) {
   var numbersof_update_sql = 'UPDATE numbersof SET visiters=visiters+1';
   connection.query(numbersof_update_sql, function(err, rows, fields) {
     if (!err){
-      res.render('index');
+      res.render('main');
     }else{
       console.log('Error while performing Query.', err);
     }
   });
 });
-router.get('/work', function(req, res, next) {
-  res.render('work');
+router.get('/facility', function(req, res, next) {
+  res.render('facility');
 });
-router.get('/work2', function(req, res, next) {
+router.get('/notice', function(req, res, next) {
   var search_sql = 'SELECT * FROM consult'
   connection.query(search_sql, function(err, rows, fields) {
     if (!err){
-      res.render('work2', {rows:rows})
+      res.render('notice', {rows:rows})
     }else{
       console.log('Error while performing Query.', err);
     }
   });
 });
-router.get('/services', function(req, res, next) {
-  res.render('services');
+router.get('/info', function(req, res, next) {
+  res.render('info');
 });
-router.get('/about', function(req, res, next) {
-  res.render('about');
-});
-router.get('/about2', function(req, res, next) {
-  res.render('about2');
-});
-router.get('/contact', function(req, res, next) {
-  res.render('contact');
+router.get('/schedule', function(req, res, next) {
+  res.render('schedule');
 });
 
 module.exports = router;
